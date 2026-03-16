7772e92f55e0abc5845a28f6f4238275e3363dd3
Romontt <rmontiel1994@gmail.com>
2026-03-16 15:33:48 -0600

    Create productos.js

---
+++ menu/Uy_ke_rico/productos.js
@@ -0,0 +1,29 @@
+const MENU_DATA = [
+    {
+        categoria: "Hamburguesas",
+        items: [
+            { id: 1, nombre: "Sencilla", precio: 2000, desc: "Pan, torta, queso, tomate, lechuga y salsas." },
+            { id: 2, nombre: "Sencilla Tocineta", precio: 2300, desc: "Pan, torta, queso, tomate, lechuga, salsas y tocineta." },
+            { id: 3, nombre: "Especial", precio: 3000, desc: "Pan, torta, queso, tomate, lechuga, salsas, tocineta, cebolla caramelizada y pepinillos." },
+            { id: 4, nombre: "Smash Burger", precio: 4000, desc: "Pan, 2 tortas, 2 quesos, cebolla caramelizada, pepinillos y salsa especial Smash." }
+        ]
+    },
+    
+    {
+        categoria: "Papas Fritas",
+        items: [
+            { id: 5, nombre: "Papas Pequeñas", precio: 1000, desc: "" },
+            { id: 6, nombre: "Papas Grandes", precio: 1500, desc: "" },
+            { id: 7, nombre: "Salchipapas", precio: 2500, desc: "" },
+            { id: 8, nombre: "Papas Especiales", precio: 4000, desc: "Papas, costilla al humo, salchicha, tocineta en BBQ, cheddar y salsas de la casa." }
+        ]
+    },
+    {
+        categoria: "Carne y Combos",
+        items: [
+            { id: 9, nombre: "Costilla (Kilo)", precio: 12000, desc: "Sal o BBQ. Pide la cantidad que guste." },
+            { id: 10, nombre: "Porción de Costilla", precio: 3500, desc: "Sal o BBQ, ensalada, banano o yuca." },
+            { id: 11, nombre: "Combo #1", precio: 5000, desc: "Hamburguesa Especial + Papas Especiales." }
+        ]
+    }
+];
