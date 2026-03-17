317afda22af984f5da39873eddaf0ae3536e3a3b
Romontt <rmontiel1994@gmail.com>
2026-03-16 18:51:00 -0600

    Update app.js

--- app.js
+++ app.js
@@ -207,7 +207,7 @@ function renderCards(listaFiltrada) {
             
             // 2. Lógica del Badge "Menú Disponible" (Posicionado en la división imagen/texto)
             const badgeMenu = esPremium ? `
-                <div class="absolute bottom-0 right-4 z-20 flex items-center gap-1.5 px-2.5 py-1.5 rounded-t-sm border-t border-x border-[#d4a373]/30 bg-[#130f0e] shadow-[0_-4px_10px_rgba(212,163,115,0.15)] transition-all duration-500 group-hover:px-4">
+                <div class="absolute bottom-0 right-0 z-20 flex items-center gap-1.5 px-2.5 py-1.5 rounded-t-sm border-t border-x border-[#d4a373]/30 bg-[#130f0e] shadow-[0_-4px_10px_rgba(212,163,115,0.15)] transition-all duration-500 group-hover:px-4">
                     <svg class="w-3 h-3 text-[#d4a373]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                     </svg>
