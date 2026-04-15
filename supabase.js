9cebad6abdf6bc4b7675553548f4b74bcda93caa
Romontt <rmontiel1994@gmail.com>
2026-04-14 22:36:05 -0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,14 +1,21 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// --- PROYECTO 1: VIDEOS Y EMPLEOS ---
+// --- CONFIGURACIÓN PROYECTO 1 ---
 const supabaseUrl = 'https://svkyczglvidntguqduej.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2a3ljemdsdmlkbnRndXFkdWVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5MDAwODEsImV4cCI6MjA5MDQ3NjA4MX0.gASHvLpE4xrSKY0ll5Votnz1oBAtrTXWnT7ww__Tdpg'
 
+// 1. Cliente para EVENTOS (Usa el esquema 'public' por defecto)
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
+// 2. Cliente para EMPLEOS (Si tus otros códigos lo buscan en el esquema 'empleos')
+export const supabaseEmpleos = createClient(supabaseUrl, supabaseAnonKey, {
+  db: { schema: 'empleos' }
+})
 
-// --- PROYECTO 2: ACTIVIDAD (Métricas) ---
+
+// --- CONFIGURACIÓN PROYECTO 2 ---
 const urlActividad = 'https://yfqxnjohojtbjevrmbmq.supabase.co'
 const keyActividad = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmcXhuam9ob2p0YmpldnJtYm1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MjI4ODgsImV4cCI6MjA4ODM5ODg4OH0.ze32GU0sW7EZ5oicnLFlHpthtLcSTUxZ9rlHSyQLFso'
 
+// 3. Cliente para MÉTRICAS
 export const supabaseActividad = createClient(urlActividad, keyActividad)
